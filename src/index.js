8e24b6fc904f3f42870da36d4ff2be460de02ff1
Thomas <tcotte061@gmail.com>
2026-02-24 16:37:24 -0500

    packaging: include manifest.json for chromium consumers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const path = require("node:path");
 
 module.exports = {
   extensionRoot: __dirname,
-  manifestPath: path.join(__dirname, "manifest_v3.json"),
+  manifestPath: path.join(__dirname, "manifest.json"),
 };
